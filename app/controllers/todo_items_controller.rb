a918b006500e4e42f2ce38137558384cd4c5661d
pnisarg <pnisarg26@yahoo.com>
2016-06-10 16:37:42 -0700

    completed styling and functionality.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
 
   before_action :set_todo_list
+  before_action :set_todo_item, :except => [:create]
 
   def create
     @todo_item = TodoItem.new(todo_item_params)
@@ -16,18 +17,26 @@ class TodoItemsController < ApplicationController
 
   def destroy
     # @todo_item = TodoItem.find(params[:id])
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
-      flash[:success] = "Todo List item was deleted."
+      flash[:notice] = "Todo List item was deleted."
     else
-      flash[:error] = "Todo List item could not be deleted."
+      flash[:notice] = "Todo List item could not be deleted."
     end
     redirect_to @todo_list
+  end
+
+  def complete
+    @todo_item.update_attributes(:completed_at => Time.now)
+    redirect_to @todo_list, :notice => "Todo item completed"
   end
 
   private
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
